e72261ba7874343ae456e5c4b25b8c26856a2ff7
Qihao Ye <11610812@mail.sustc.edu.cn>
2018-09-15 01:40:35 +0800

    Minor change

--- src/client.py
+++ src/client.py
@@ -1,43 +1,46 @@
 import socket
 import threading
 import select
-import sys
+
 
 def connect_to_server(addr):
-	server_socket = socket.socket()
-	server_socket.connect(addr)
-	return server_socket
+    server_socket = socket.socket()
+    server_socket.connect(addr)
+    return server_socket
+
 
 def listen_to_server(server_socket):
-	listening = [server_socket]
-	while True:
-		active_socket_list,w,e = select.select(listening,[],[])
-		if server_socket in active_socket_list:
-			try:
-				print(server_socket.recv(1024).decode('utf-8'))
-			except socket.error as e:
-				print(e,' Failed to recieve message from server.')
+    listening = [server_socket]
+    while True:
+        active_socket_list, w, e = select.select(listening, [], [])
+        if server_socket in active_socket_list:
+            try:
+                print(server_socket.recv(1024).decode('utf-8'))
+            except socket.error as e:
+                print(e, ' Failed to receive message from server.')
+
 
 def send_to_server(server_socket):
-	while True:
-		try:
-			print('>>>',end='')
-			msg = input()
-			if(msg=='$exit'):
-				server_socket.sendall(msg.encode())
-		except Exception as e:
-			print(e)
-		try:
-			server_socket.send((msg).encode())
-		except Exception as e:
-			print(e)
+    while True:
+        try:
+            print('>>>', end='')
+            msg = input()
+            if msg == '$exit':
+                server_socket.sendall(msg.encode())
+        except Exception as e:
+            print(e)
+        try:
+            server_socket.send(msg.encode())
+        except Exception as e:
+            print(e)
+
 
 if __name__ == '__main__':
-	server_ip = input('Server IP:')
-	server_port = int(input('Server Port:'))
-	print('\n')
-	server_socket = connect_to_server((server_ip,server_port))
-	listen_thr = threading.Thread(target=listen_to_server,args=(server_socket,))
-	send_thr = threading.Thread(target=send_to_server,args=(server_socket,))
-	listen_thr.start()
-	send_thr.start()
+    server_ip = input('Server IP:')
+    server_port = int(input('Server Port:'))
+    print('\n')
+    server_socket = connect_to_server((server_ip, server_port))
+    listen_thr = threading.Thread(target=listen_to_server, args=(server_socket,))
+    send_thr = threading.Thread(target=send_to_server, args=(server_socket,))
+    listen_thr.start()
+    send_thr.start()
